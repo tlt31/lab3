0656ade3f176e537ca98a319a1dc2ac9690a189d
tim <tlt31@uakron.edu>
2015-02-18 19:43:45 -0500

    021615:0900

--- src/lab3.java
+++ src/lab3.java
@@ -7,21 +7,49 @@ public class lab3 {
 
     public static void main(String[] args) {
 
-        //Declare variables
-        int air, water, steel, medium, time;
-        String input;
+        final int air = 1100, water = 4900, steel = 16400;
+        float medium = 0, inputD = 0;
+        float time;
+        String input = "";
 
-        //Add Scanner
         Scanner scan = new Scanner(System.in);
 
-        //Ask user to enter air, water, or steel
-        System.out.println("Please enter the medium you are calclulating for.\nThis can be be air, water, or steel.");
-        input = scan.nextLine();
-        if (input == "Air" || input =="air") {
-            System.out.println("You selected air");
-        } else {
-            System.out.println("You have not selected a valid option");
+        boolean error = true;
+        while (error) {
+            System.out.println("Please enter the medium you are calculating for.\nThis can be be air, water, or steel.");
+            input = scan.nextLine();
+            if (input.equals("air")) {
+                System.out.println("you have entered air");
+                medium = air;
+                error = false;
+            } else if (input.equals("water")) {
+                System.out.println("you have entered water");
+                medium = water;
+                error = false;
+            } else if (input.equals("steel")) {
+                System.out.println("you have entered steel");
+                medium = steel;
+                error = false;
+            } else {
+                System.out.println("you have not entered a valid selection");
+            }
         }
+
+        error = true;
+        while (error) {
+            System.out.println("Please enter the distance you are calculating for.\nThis should be in feet.");
+            inputD = scan.nextInt();
+            if (inputD <= 0) {
+                System.out.println("you have entered a distance less than 0.  Try again");
+                error = true;
+            } else {
+                System.out.println("you are calculating for " + inputD + " feet");
+                error = false;
+            }
+        }
+
+        time = inputD/medium;
+        System.out.println("It will take ~" + time + " seconds for sound to travel " + inputD + "ft through " + input + ".");
 
 
     }
---
+++ src/lecture.java
@@ -0,0 +1,15 @@
+/**
+ * Created by tim on 2/16/15.
+ */
+public class lecture {
+
+    public static void main (String[] args){
+
+        int i = 0;
+
+        while ( i < 5 ){
+            System.out.println(i);
+            i++;
+        }
+    }
+}
